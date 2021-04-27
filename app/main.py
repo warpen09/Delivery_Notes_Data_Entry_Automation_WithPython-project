ffa22bfc32e51fb7152412414e152fa5b0ce20db
Hung Le <hunghaile@gmail.com>
2021-04-26 19:56:27 -0500

    creating ini files

--- app/main.py
+++ app/main.py
@@ -20,12 +20,25 @@ for filename in pdfFiles:
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     print(filename)
 '''
+def create_ini_files(path,pdf_files):
+    for pdf_file in pdf_files:
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in [f for f in filenames if f.endswith(".pdf")]:
+                print(os.path.splitext(os.path.basename(pdf_file))[0])
+                open(os.path.join(dirpath, '%s.ini' ) % (os.path.splitext(os.path.basename(pdf_file))[0],),'w')
+        #for dirpath, dirnames, filenames in os.walk(pdf_file):
+        #    base=os.path.basename(pdf_file)
+        #    open(os.path.join(path,'Data/%s.ini') % (os.path.splitext(base))[0], 'w')
 
 def look_for_pdf(path, pdf_files):
     for dirpath, dirnames, filenames in os.walk(path):
         for filename in [f for f in filenames if f.endswith(".pdf")]:
-            print(filename)
+            #print(filename)
             pdf_files.append(os.path.join(dirpath, filename))
+    #pdf_files
+    create_ini_files(path,pdf_files)
+
+
 
 
 def main():
